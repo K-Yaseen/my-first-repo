bd21621ec26bd72d30451514a39679bf2b03c9b6
Khaled Yaseen <yaseen.designservice@gmail.com>
2025-02-16 18:57:28 +0100

    تحديث تلقائي

--- project-folder/frontend/user.js
+++ project-folder/frontend/user.js
@@ -514,12 +514,4 @@ function decreaseQuantity() {
   }
 }
 
-function sendOrder() {
-  const quantity = document.getElementById("quantityValue").innerText;
-  const itemName = document.getElementById("itemName").innerText;
-  // تعديل الرسالة بحيث يظهر عدد الطلبات بجانب اسم الصنف
-  const message = "Bestellung: " + itemName + " (Anzahl: " + quantity + ")";
-  // Stellen Sie sicher, dass Sie 'رقم_المطعم' durch die tatsächliche WhatsApp-Nummer des Restaurants ersetzen.
-  const whatsappURL = "https://wa.me/رقم_المطعم?text=" + encodeURIComponent(message);
-  window.open(whatsappURL, "_blank");
-}
+
