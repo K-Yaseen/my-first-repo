86caecfa60902ebee36ba8a38c83fa854cf977e2
Khaled Yaseen <yaseen.designservice@gmail.com>
2025-02-17 11:30:48 +0100

    تحديث تلقائي

--- project-folder/frontend/user.js
+++ project-folder/frontend/user.js
@@ -427,7 +427,7 @@ async function sendToWhatsApp() {
         const quantity = quantitySelectEl ? quantitySelectEl.value : "1";
 
         // عرض كل عنصر مع كميته
-        message += `- ${itemText}\n`;
+        message += `- ${itemText}\n (${quantity})`;
       });
       message += "\n";
     }
