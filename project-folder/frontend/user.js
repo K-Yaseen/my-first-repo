fc1a4fff8e0ccb07a3da77ff6b5b119a621b27ea
Khaled Yaseen <yaseen.designservice@gmail.com>
2025-02-18 00:24:58 +0100

    تحديث تلقائي

--- project-folder/frontend/user.js
+++ project-folder/frontend/user.js
@@ -615,7 +615,7 @@ function updateFloatingCart(item, quantity = 1) {
     </svg>`;
   deleteBtn.title = "Gericht Löschen";
   deleteBtn.addEventListener("click", function () {
-    if (confirm("ه)) {
+    if (confirm("هل تريد حقًا إزالة هذا الصنف من السلة؟")) {
       li.remove();
       updateCartButton();
       saveCart();
